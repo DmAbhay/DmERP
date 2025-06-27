8e0b52a70b25709d3997f307339b37497da59ccf
DmAbhay <abhaykumar.pandey@dataman.in>
2025-06-27 17:21:48 +0530

    I have completed

--- AuthService/src/main/java/dataman/jwt/JwtAuthenticationFilter.java
+++ AuthService/src/main/java/dataman/jwt/JwtAuthenticationFilter.java
@@ -32,6 +32,90 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String username = null;
         String jwt = null;
+        NamedParameterJdbcTemplate npjt = null;
+
+        String requestURI = request.getRequestURI();
+
+
+        if ("/api/datamanerp/myconfig3/centralized-login-new".equals(requestURI)) {
+            System.out.println(">>> Centralized login API called!");
+        }
+
+        //String[] ans = "/api/datamanerp/myconfig3/centralized-login-new".substring(1).split("/");
+
+        String[] ans = request.getRequestURI().substring(1).split("/");
+
+        String configName = null;
+        if(requestURI.contains("/centralized-login-new")){
+            configName = ans[2]+ ".properties";
+
+
+            File file = new File(CONFIG_DIR, configName);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            try (FileInputStream fis = new FileInputStream(file)) {
+                Properties props = new Properties();
+                props.load(fis);
+
+                DataSource ds = dynamicBuilder.buildDataSource(props);
+                JdbcTemplate jdbc = dynamicBuilder.buildJdbcTemplate(ds);
+
+                npjt = dynamicBuilder.buildNamedJdbcTemplate(ds);
+                String dbName = jdbc.queryForObject("SELECT DB_NAME()", String.class);
+
+                System.out.println("database Name hai "+dbName);
+                System.out.println("jai ho");
+
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }else{
+
+            String token = authorizationHeader.substring(7);
+            username = jwtTokenUtil.extractUsername(token);
+            String key = username + "_" + token;
+            configName = redisObjectUtil.getObjectValueAsString(key, "configName");
+            System.out.println("I have successfully retrived the configName from redis "+configName);
+
+            Object redisValue = redisObjectUtil.getObjectValue(key, "configProperties");
+
+            Properties prop = new Properties();
+
+            if (redisValue instanceof Map<?, ?> map) {
+                for (Map.Entry<?, ?> entry : map.entrySet()) {
+                    prop.put(entry.getKey().toString(), entry.getValue().toString());
+                }
+            } else {
+                throw new IllegalArgumentException("Unexpected object type from Redis: " + redisValue.getClass());
+            }
+            Debug.printDebugBoundary("\uD83D\uDC96");
+            System.out.println(prop);
+            Debug.printDebugBoundary("\uD83D\uDC96");
+
+            if(prop.isEmpty()){
+                throw new DmException(1000, "Property not loaded from redis");
+            }
+
+
+            try{
+                DataSource ds = dynamicBuilder.buildDataSource(prop);
+                JdbcTemplate jdbc = dynamicBuilder.buildJdbcTemplate(ds);
+
+                npjt = dynamicBuilder.buildNamedJdbcTemplate(ds);
+                String dbName = jdbc.queryForObject("SELECT DB_NAME()", String.class);
+
+                System.out.println("Database Name kya hai"+dbName);
+            }catch (Exception ex) {
+                throw new DmException(1001, "Error: Connection Failed");
+            }
+        }
+
+        System.out.println(Arrays.toString(ans));
+
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
